9b16e8dd5fb894bb592103f1f5465448d197545d
xcstream <xcstream@vip.qq.com>
2016-05-05 16:36:09 +0800

    add reply function，say to last speaker

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ const repl = require('repl');
 const myteleid =  ''+Math.floor(Math.random() * 8999+1000);
 const avatars = require('./avatar')();
 
+
 console.log('my teleid',myteleid,"\nusage: send('text','peer teleid')");
 var myavatar = avatars[Math.floor(Math.random() * 999) % avatars.length]
 
 var replcontext = repl.start(`[${myteleid}]${myavatar}> `).context;
 
+replcontext.lastsender = ""
 replcontext.send = function(text,peerteleid){
   client.publish(''+peerteleid,`[${myteleid}]${myavatar} says: ${text}`);
+};
+
+replcontext.reply = function(text){
+  if(replcontext.lastsender!=""){
+    replcontext.send(text,replcontext.lastsender)
+  }else{
+    console.log('no target to reply.')
+  }
 };
 
 client.on('connect', function () {
@@ -19,5 +29,11 @@ client.on('connect', function () {
 
 client.on('message', function (topic, message) {
   var msgstring = message.toString()
-  console.log(`\n\x1b[22;32m [${(new Date).toString()}]\n\x1b[32;32m ${msgstring}\x1b[0m`)
+  savesender(msgstring)
+  console.log(`\n\x1b[22;32m [${(new Date).toString()}]\n\x1b[22;36m ${msgstring}\x1b[0m`)
 });
+
+function savesender(msgstring){
+  replcontext.lastsender = msgstring.substr(1,4)
+  console.log(replcontext.lastsender)
+}
